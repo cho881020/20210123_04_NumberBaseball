d19b1ca2a09d71ad8e2722be6a68558f968e3b64
조경진 <cho881020@gmail.com>
2021-01-23 14:15:45 +0900

    입력값을 3칸의 배열로 분해

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -20,6 +20,20 @@ public class MainDrive {
 			System.out.print("3자리 숫자 입력 : ");
 			int input = scan.nextInt();
 			
+//			배열 {7,4,1} Vs. 입력값 741 => {7,4,1} 로 분해 작업 필요.
+			
+			int[] myNumbers = new int[3];
+			
+//			myNumbers의 0번칸 : 입력값의 100의자리.
+			myNumbers[0] = input / 100;
+			
+//			myNumbers의 1번칸 : 입력값의 10의자리. => 741 / 10 % 10
+			myNumbers[1] = input / 10 % 10;
+			
+//			myNumbers의 2번칸 : 입력값의 1의자리.
+			myNumbers[2] = input % 10;
+			
+			
 		}
 		
 	}
